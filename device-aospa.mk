35a9de8627645645fc3840212a6a2e0ace208e7b
Ethan Chen <intervigil@gmail.com>
2018-12-01 13:53:00 -0800

    floral: Declare EUICC permission
    This is a squash of the following:
    
    Author: Ethan Chen <intervigil@gmail.com>
    Date:   Sat Dec 1 13:53:00 2018 -0800
    
        floral: Declare EUICC permission
    
        Change-Id: I1b151021ca9fc6cf35141b2ccb337bc83197089e
    
    Author: razorloves <razorloves@gmail.com>
    Date:   Fri Oct 18 00:06:22 2019 -0500
    
        floral: Move eUICC permissions xml to /product
    
        All eUICC items were moved to /product in Q.
    
        Change-Id: I614f86b16f61959c47465863ab1d5d55030209ab
    
    Change-Id: I323dce8b848b44e45ed21002950059e58719113c

--- device-aospa.mk
+++ device-aospa.mk
@@ -4,6 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# EUICC
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
+
 # Overlay
 PRODUCT_PACKAGES += \
     AOSPAFloralFrameworksOverlay

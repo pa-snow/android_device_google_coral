e217c7649a4e045db4efc88e25715edb7954989f
razorloves <razorloves@gmail.com>
2020-05-12 23:50:50 -0500

    floral: Set slotId# of non-removable esim
    This is needed for esim to function properly.
    Without it the EID value could not be read.
    
    Change-Id: I414a7c98362523c5880cfa90b6ce9c0ce92e9c32

--- device-aospa.mk
+++ device-aospa.mk
@@ -4,5 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Overlay
+PRODUCT_PACKAGES += \
+    AOSPAFloralFrameworksOverlay
+
 # Prebuilt Kernel
 $(call inherit-product, device/google/coral-kernel/kernel.mk)
